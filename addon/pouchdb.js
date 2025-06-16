c67feae127f2366dad95e4be87a217f553a3c0d6
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-06-16 17:28:16 -0300

    Directly exports the extended `PouchDb` class returned by `.plugin()` calls

--- addon/pouchdb.js
+++ addon/pouchdb.js
@@ -2,6 +2,4 @@ import PouchDB from 'pouchdb-browser';
 import PouchDBFind from 'pouchdb-find';
 import PouchDBRelational from 'relational-pouch';
 
-PouchDB.plugin(PouchDBFind).plugin(PouchDBRelational);
-
-export default PouchDB;
+export default PouchDB.plugin(PouchDBFind).plugin(PouchDBRelational);
