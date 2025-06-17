1dc84d8ace27bc0347f4b2fc8c8f70f8e00f3734
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-06-17 18:23:13 -0300

    Modernizes some syntax.

--- tests/integration/serializers/pouch-test.js
+++ tests/integration/serializers/pouch-test.js
@@ -6,19 +6,19 @@ module('Integration | Serializer | Attachments', function (hooks) {
   setupTest(hooks);
   moduleForIntegration(hooks);
 
-  let id = 'E';
-  let coverImage = {
+  const id = 'E';
+  const coverImage = {
     name: 'cover.jpg',
     content_type: 'image/jpeg',
     data: window.btoa('cover.jpg'),
     length: 9,
   };
-  let photo1 = {
+  const photo1 = {
     name: 'photo-1.jpg',
     content_type: 'image/jpeg',
     data: window.btoa('photo-1.jpg'),
   };
-  let photo2 = {
+  const photo2 = {
     name: 'photo-2.jpg',
     content_type: 'image/jpeg',
     data: window.btoa('photo-2.jpg'),
@@ -33,15 +33,11 @@ module('Integration | Serializer | Attachments', function (hooks) {
     await newRecipe.save();
 
     const newDoc = await this.db().get('tacoRecipe_2_E');
+    const attachments = newDoc._attachments;
 
-    function checkAttachment(attachments, fileName, value, message) {
-      delete attachments[fileName].revpos;
-      assert.deepEqual(attachments[fileName], value, message);
-    }
-
-    checkAttachment(
-      newDoc._attachments,
-      'cover.jpg',
+    delete attachments[coverImage.name].revpos;
+    assert.deepEqual(
+      attachments[coverImage.name],
       {
         digest: 'md5-SxxZx3KOKxy2X2yyCq9c+Q==',
         content_type: 'image/jpeg',
@@ -50,30 +46,25 @@ module('Integration | Serializer | Attachments', function (hooks) {
       },
       'attachments are placed into the _attachments property of the doc',
     );
-
     assert.deepEqual(
-      Object.keys(newDoc._attachments).sort(),
+      Object.keys(attachments).sort(),
       [coverImage.name, photo1.name, photo2.name].sort(),
       'all attachments are included in the _attachments property of the doc',
     );
-
     assert.true(
       'cover_image' in newDoc.data,
       'respects the mapping provided by the serializer `attrs`',
     );
-
     assert.deepEqual(
       newDoc.data.cover_image,
       { 'cover.jpg': { length: 9 } },
       'the attribute contains the file name',
     );
-
     assert.strictEqual(
       newDoc.data.cover_image['cover.jpg'].length,
       9,
       'the attribute contains the length to avoid empty length when File objects are saved and have not been reloaded',
     );
-
     assert.deepEqual(
       newDoc.data.photo_gallery,
       { 'photo-1.jpg': {}, 'photo-2.jpg': {} },
@@ -84,7 +75,7 @@ module('Integration | Serializer | Attachments', function (hooks) {
     assert.strictEqual(recordInStore.coverImage.name, coverImage.name);
     assert.strictEqual(recordInStore.coverImage.data, coverImage.data);
 
-    const photosAttr = recordInStore.photos;
+    const photosAttr = await recordInStore.photos;
     assert.strictEqual(photosAttr.length, 2, '2 photos');
     assert.strictEqual(photosAttr[0].name, photo1.name);
     assert.strictEqual(photosAttr[0].data, photo1.data);
