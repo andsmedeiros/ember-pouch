8b13d795c8b712f82f1d2a56de5da77859437ec3
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-06-17 14:10:38 -0300

    Better consistency of addon exports
    - Moves `PouchModel` to `models/pouch.js`
    - Exports the model from the new path
    - Adds a model export from `/app`
    - Exports `PouchDB`, with plugins already attached, directly from the addon

--- addon/index.js
+++ addon/index.js
@@ -1,5 +1,6 @@
-import Model from './model';
+import Model from './models/pouch';
 import Adapter from './adapters/pouch';
 import Serializer from './serializers/pouch';
+import PouchDB from './pouchdb';
 
-export { Model, Adapter, Serializer };
+export { Model, Adapter, Serializer, PouchDB };
