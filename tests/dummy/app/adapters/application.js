fd68f0a2c2cd943838f1bf5bd4e8a08638dc5ae7
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-06-23 10:58:16 -0300

    Moves `unloadedDocumentChanged` from `adapter:application` to `adapter:taco-salad`

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -64,11 +64,4 @@ export default class ApplicationAdapter extends Adapter {
       });
     });
   }
-
-  async unloadedDocumentChanged(obj) {
-    const recordModel = this.store.modelFor(obj.type);
-    const recordTypeName = this.getRecordTypeName(recordModel);
-    const doc = await this.db.rel.find(recordTypeName, obj.id);
-    await this.store.pushPayload(recordTypeName, doc);
-  }
 }
--- tests/dummy/app/adapters/taco-salad.js
+++ tests/dummy/app/adapters/taco-salad.js
@@ -1,3 +1,10 @@
 import ApplicationAdapter from './application';
 
-export default class TacoSaladAdapter extends ApplicationAdapter {}
+export default class TacoSaladAdapter extends ApplicationAdapter {
+  async unloadedDocumentChanged(obj) {
+    const recordModel = this.store.modelFor(obj.type);
+    const recordTypeName = this.getRecordTypeName(recordModel);
+    const doc = await this.db.rel.find(recordTypeName, obj.id);
+    await this.store.pushPayload(recordTypeName, doc);
+  }
+}
