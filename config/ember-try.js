9c2251851a17f4ead3dee9a76e6558c61ba8e022
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-06-26 13:32:59 -0300

    Implements test scenarios for Ember/EmberData >=5.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,4 +1,6 @@
 'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
 const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
@@ -6,38 +8,83 @@ module.exports = async function () {
     packageManager: 'pnpm',
     scenarios: [
       {
-        name: 'ember-4.0',
+        name: 'ember-5.0',
         npm: {
           devDependencies: {
-            'ember-source': '~4.0.0',
-            'ember-data': '~4.0.0',
+            'ember-source': '~5.0.0',
+            'ember-data': '~5.0.0',
           },
         },
       },
       {
-        name: 'ember-lts-4.4',
+        name: 'ember-lts-5.4',
         npm: {
           devDependencies: {
-            'ember-source': '~4.4.0',
-            'ember-data': '~4.4.0',
+            'ember-source': '~5.4.0',
+            'ember-data': '~5.3.0',
           },
         },
       },
       {
-        name: 'ember-lts-4.8',
+        name: 'ember-lts-5.8',
         npm: {
           devDependencies: {
-            'ember-source': '~4.8.0',
-            'ember-data': '~4.8.0',
+            'ember-source': '~5.8.0',
+            'ember-data': '~5.3.0',
           },
         },
       },
       {
-        name: 'ember-lts-4.12',
+        name: 'ember-lts-5.12',
         npm: {
           devDependencies: {
-            'ember-source': '~4.12.0',
-            'ember-data': '~4.12.0',
+            'ember-source': '~5.12.0',
+            'ember-data': '~5.3.0',
+          },
+        },
+      },
+      {
+        name: 'ember-6.0',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.0.0',
+            'ember-data': '~5.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-6.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~6.4.0',
+            'ember-data': '~5.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-data': 'latest',
+            'ember-source': await getChannelURL('release'),
+          },
+        },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-data': 'beta',
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-data': 'canary',
+            'ember-source': await getChannelURL('canary'),
           },
         },
       },
