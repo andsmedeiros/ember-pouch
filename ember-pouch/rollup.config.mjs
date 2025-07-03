f3fae7f3be50ba26aa74ca4193da197a581144f2
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-07-03 12:37:57 -0300

    Removes unused artifact directives. Correctly defines entry points.

--- ember-pouch/rollup.config.mjs
+++ ember-pouch/rollup.config.mjs
@@ -20,18 +20,7 @@ export default {
     // up your addon's public API. Also make sure your package.json#exports
     // is aligned to the config here.
     // See https://github.com/embroider-build/embroider/blob/main/docs/v2-faq.md#how-can-i-define-the-public-exports-of-my-addon
-    addon.publicEntrypoints(['**/*.js', 'index.js']),
-
-    // These are the modules that should get reexported into the traditional
-    // "app" tree. Things in here should also be in publicEntrypoints above, but
-    // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports([
-      'components/**/*.js',
-      'helpers/**/*.js',
-      'modifiers/**/*.js',
-      'services/**/*.js',
-      'utils/**/*.js',
-    ]),
+    addon.publicEntrypoints(['index.js', 'pouchdb.js', 'transforms/*.js']),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
@@ -48,16 +37,6 @@ export default {
       babelHelpers: 'bundled',
       extensions: ['.js', '.gjs'],
     }),
-
-    // Ensure that standalone .hbs files are properly integrated as Javascript.
-    addon.hbs(),
-
-    // Ensure that .gjs files are properly integrated as Javascript
-    addon.gjs(),
-
-    // addons are allowed to contain imports of .css files, which we want rollup
-    // to leave alone and keep in the published output.
-    addon.keepAssets(['**/*.css']),
 
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
