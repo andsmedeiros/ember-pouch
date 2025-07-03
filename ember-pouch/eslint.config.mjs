e3d3b6f6f4e6816b4da5b4363870682eca2755a5
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-07-03 15:03:39 -0300

    Updates ESLint config file - Ignores variables and arguments starting with `_` - Marks blueprint exports as commonjs

--- ember-pouch/eslint.config.mjs
+++ ember-pouch/eslint.config.mjs
@@ -1,1 +1,32 @@
-export { default } from '@ijlee2-frontend-configs/eslint-config-ember/v2-addon';
+import defaultConfig from '@ijlee2-frontend-configs/eslint-config-ember/v2-addon';
+import node from 'eslint-plugin-n';
+
+export default [
+  ...defaultConfig,
+  {
+    ignores: [
+      'blueprints/*/files/',
+      'dist/'
+    ],
+  },
+
+  {
+    rules: {
+      'no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+
+  {
+    files: [ './blueprints/*/index.js' ],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+    ...node.configs['flat/recommended-script']
+  },
+]
