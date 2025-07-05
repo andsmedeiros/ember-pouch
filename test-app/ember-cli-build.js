94a1b13f444fa7410fc3c10e1d1f547de5f59b71
André Medeiros <andre@setadesenvolvimentos.com.br>
2025-07-05 11:13:20 -0300

    Resolves store extending EmberObject deprecation

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -15,6 +15,11 @@ module.exports = function (defaults) {
         watching: ['ember-pouch'],
       }),
     },
+    emberData: {
+      deprecations: {
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+      }
+    },
   });
 
   return maybeEmbroider(app);
